9e00e36ed397fcd1ba1146f5cf80cc8761c2c1b4
KamilSiwek <kontokamilsiwek@gmail.com>
2018-09-12 15:40:30 +0200

    Add save and load files

--- lib/cards.ex
+++ lib/cards.ex
@@ -22,26 +22,13 @@ defmodule Cards do
     "hi there!"
   end
 
-  # def create_deck do
-  #   values = ["Ace", "Two", "Three", "Three", "Four", "Five"]
-  #   suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
-  #
-  #   cards = for value <- values do
-  #     for suit <- suits do
-  #       "#{value} of #{suit}"
-  #     end
-  #   end
-  #
-  #   List.flatten(cards)
-  # end
-
   def create_deck do
     values = ["Ace", "Two"]
     suits = ["Spades", "Clubs"]
 
-      for value <- values, suit <- suits do
-        "#{value} of #{suit}"
-      end
+    for value <- values, suit <- suits do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
@@ -60,4 +47,12 @@ defmodule Cards do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+
 end
