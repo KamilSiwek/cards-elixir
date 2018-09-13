3675eb3f7e4e60f84749766b8c5bd005e34e1174
KamilSiwek <kontokamilsiwek@gmail.com>
2018-09-13 10:44:27 +0200

    Add tests

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,26 +1,17 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for Cards.
+  Provides methods for creating and gandling a deck of cards.
   """
 
   @doc """
-  Hello world.
+  Returns a list of strings representing a deck of playing cards.
 
   ## Examples
-
-      iex> Cards.hello()
-      "hi there!"
 
       iex> Cards.create_deck()
       ["Ace of Spades", "Ace of Clubs", "Two of Spades", "Two of Clubs"]
 
-      iex> Cards.contains?(["Ace", "Two", "Three"], "Two")
-      true
-
   """
-  def hello do
-    "hi there!"
-  end
 
   def create_deck do
     values = ["Ace", "Two"]
@@ -31,22 +22,69 @@ defmodule Cards do
     end
   end
 
+  @doc """
+  Returns shuffled data.
+
+  """
+
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
+
+  @doc """
+  Returns true if `deck` contains `card`.
+
+  ## Examples
+
+      iex> Cards.contains?(["Ace", "Two", "Three"], "Two")
+      true
+
+  """
 
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+  Dicides a deck into a hand the remainder of the deck.
+
+  ## Examples
+
+      iex> Cards.deal(["Ace of Spades", "Ace of Clubs", "Two of Spades", "Two of Clubs"], 2)
+      {["Ace of Spades", "Ace of Clubs"], ["Two of Spades", "Two of Clubs"]}
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
+  @doc """
+  Save data to file.
+
+  ## Examples
+
+      iex> Cards.save(["Ace of Spades", "Ace of Clubs", "Two of Spades", "Two of Clubs"], 'my_deck')
+      :ok
+
+  """
 
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
+
+  @doc """
+  Returns data from file.
+
+  ## Examples
+
+      iex> Cards.load("my_deck")
+      ["Ace of Spades",
+      "Ace of Clubs",
+      "Two of Spades",
+      "Two of Clubs"]
+
+  """
 
   def load(filename) do
     case File.read(filename) do
@@ -54,6 +92,11 @@ defmodule Cards do
       {:error, _reason} -> "That file does not exist"
     end
   end
+
+  @doc """
+  Return two arrays. First array has the indicated amount of items.
+
+  """
 
   def create_hand(hand_size) do
     # deck = Cards.create_deck
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,10 +2,6 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == "hi there!"
-  end
-
   test "Numbers" do
     assert Cards.create_deck() == ["Ace of Spades", "Ace of Clubs", "Two of Spades", "Two of Clubs"]
   end
@@ -14,4 +10,11 @@ defmodule CardsTest do
     assert Cards.contains?(["Ace", "Two", "Three"], "Two") == true
   end
 
+  test "Deal data" do
+    assert Cards.deal(["Ace of Spades", "Ace of Clubs", "Two of Spades", "Two of Clubs"], 2) == {["Ace of Spades", "Ace of Clubs"], ["Two of Spades", "Two of Clubs"]}
+  end
+
+  test "Save data to file" do
+    assert Cards.save(["Ace of Spades", "Ace of Clubs", "Two of Spades", "Two of Clubs"], 'my_deck') == :ok
+  end
 end
